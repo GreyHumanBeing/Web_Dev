f6a1718ef5c3df4ba247c8930d1b43c71f0a3b1a
GreyHumanBeing <83623527+GreyHumanBeing@users.noreply.github.com>
2021-09-03 17:51:43 -0300

    Delete js directory

--- js/1-holamundo.js
+++
@@ -1,9 +0,0 @@
-//Activamos el modo estricto
-'use strict'
-
-alert("Hola Mundo !!");
-alert("Escrito por GreyHumanBeing");
-document.write("Hola mundo desde holamundo.js");
-console.log("Mensaje enviado desde holamundo.js"); 
-console.log("A continuacion sumamos 20 + 8 :")
-console.log(20+8);
